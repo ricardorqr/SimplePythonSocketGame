ea5052e744fb33272f5203cd782e1ebb9a868542
Ricardo Ribeiro <ricardorqr@gmail.com>
2020-05-17 19:41:15 -0400

    Updated the class client.py

--- client.py
+++ client.py
@@ -2,36 +2,43 @@ import pygame
 from network import Network
 
 
-width = 500
-height = 500
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Client')
+class Client:
+
+    def __init__(self, client_number, width=500, height=500):
+        self.client_number = client_number
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Client ' + str(client_number))
+
+    def __redraw_window(self, window, player1, player2):
+        window.fill((255, 255, 255))
+        player1.draw(window)
+        player2.draw(window)
+        pygame.display.update()
+
+    def run(self):
+        run = True
+        network = Network()
+        player1 = network.get_player()
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            player2 = network.send(player1)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+
+            player1.move()
+            self.__redraw_window(self.window, player1, player2)
 
 
-def redraw_window(window, player1, player2):
-    window.fill((255, 255, 255))
-    player1.draw(window)
-    player2.draw(window)
-    pygame.display.update()
+if __name__ == "__main__":
+    client1 = Client(1)
+    client1.run()
 
-
-def main():
-    run = True
-    network = Network()
-    player1 = network.get_player()
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(60)
-        player2 = network.send(player1)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-        player1.move()
-        redraw_window(window, player1, player2)
-
-
-main()
+    # client2 = Client(2)
+    # client2.run()
