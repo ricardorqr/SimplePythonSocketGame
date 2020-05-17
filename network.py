230e20856e2cbbec3343b6fa021999bd244cb57f
Ricardo Ribeiro <ricardorqr@gmail.com>
2020-05-17 19:42:16 -0400

    Updated the class network.py

--- network.py
+++ network.py
@@ -6,7 +6,7 @@ class Network:
 
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = '192.168.0.25'
+        self.server = '192.168.0.25'  # My internal IP
         self.port = 5555
         self.address = (self.server, self.port)
         self.player = self.connect()
